36e3ab7a4fb62578b5173e6b51b3d404b24cb824
Animeology <joseph.nguyen19@yahoo.com>
2023-06-25 16:17:42 -0500

    Create Properties

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -3,12 +3,25 @@ using System.Windows.Forms;
 
 namespace SuperAdventure
 {
-    public partial class Form1 : Form
+    public partial class SuperAdventure : Form
     {
         Player _player;
-        public Form1()
+        public SuperAdventure()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHP = 100;
+            _player.MaxHP = 100;
+            _player.Gold = 100;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHP.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
