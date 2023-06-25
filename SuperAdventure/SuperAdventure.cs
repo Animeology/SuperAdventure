5eda98268da96653943c0f4b2f55c25ad792ea92
Animeology <joseph.nguyen19@yahoo.com>
2023-06-25 16:07:07 -0500

    Create button and test

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -16,5 +16,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
     }
 }
