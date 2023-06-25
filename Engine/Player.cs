7fbf47131d9d3a1782332f1a9dce23b9fef18ef2
Animeology <joseph.nguyen19@yahoo.com>
2023-06-25 16:09:23 -0500

    Create Player.cs

---
+++ Engine/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHP { get; set; }
+        public int MaxHP { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+
+    }
+}
