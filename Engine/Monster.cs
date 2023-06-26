89f02ffe7439efbd9754e06fe0db9aed18822fbf
Animeology <joseph.nguyen19@yahoo.com>
2023-06-26 13:54:43 -0500

    Create Monster.cs

---
+++ Engine/Monster.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine
+{
+    public class Monster
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxHP { get; set; }
+        public int CurrentHP { get; set; }
+        public int MaxDamage { get; set; }
+        public int RewardXP { get; set; }
+        public int RewardDamage { get; set; }
+    }
+}
