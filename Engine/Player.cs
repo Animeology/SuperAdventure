8266385fca9bea006e4462f0cdba193fce066c79
Animeology <joseph.nguyen19@yahoo.com>
2023-06-26 17:32:02 -0500

    Add inventory and quest list

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,16 +1,23 @@
-﻿namespace Engine
+﻿using System.Collections.Generic;
+
+namespace Engine
 {
     public class Player : LivingCreature
     {
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
+
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> QuestList { get; set; }
         public Player(int currentHp, int maxHP, int gold, int experiencePoints, int level)
             : base(currentHp, maxHP)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+            Inventory = new List<InventoryItem>();
+            QuestList = new List<PlayerQuest>();
         }
     }
 }
