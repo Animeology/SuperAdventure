ef17f4b288e479e68862e82ebe4d9610b3b91d9e
Animeology <joseph.nguyen19@yahoo.com>
2023-06-26 17:23:30 -0500

    Create Inventory.cs

---
+++ Engine/Inventory.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine
+{
+    public class Inventory
+    {
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+        public Inventory(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+    }
+}
