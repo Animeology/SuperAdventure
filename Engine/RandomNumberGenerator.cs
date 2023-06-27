306638e6e33c31727805965266814fb1418ebd3b
Animeology <joseph.nguyen19@yahoo.com>
2023-06-27 18:43:29 -0500

    Create RandomNumberGenerator.cs

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Engine
+{
+    public class RandomNumberGenerator
+    {
+        static Random generator = new Random();
+
+        public int NumberRange(int min, int max)
+        {
+            return generator.Next(min, max);
+        }
+    }
+}
